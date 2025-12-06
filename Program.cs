b36f6102fe4c787a9e1843d754a2df2562a84226
Nnrlka <autkulov65@gmail.com>
2025-12-06 12:38:57 +0500

    sdf

--- Program.cs
+++ Program.cs
@@ -1,18 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace p511_oop
+class Program
 {
-    internal class Program
+    static void Main()
     {
+        PrintHeader();
 
-        static void Main(string[] args)
+        double number1 = ReadNumber("Введите первое число: ");
+        double number2 = ReadNumber("Введите второе число: ");
+        char operation = ReadOperation("Введите символ операции: ");
+
+        double result = Calculate(number1, number2, operation);
+        
+        PrintResult(number1, number2, operation, result);
+    }
+    static void PrintHeader()
+    {
+        Console.WriteLine("-- Улучшенный калькулятор ---");
+        Console.WriteLine("Доступные операции: +, -, *, /");
+    }
+    static double ReadNumber(string message)
+    {
+        Console.Write(message);
+        return Convert.ToDouble(Console.ReadLine());
+    }
+    static char ReadOperation(string message)
+    {
+        Console.Write(message);
+        return Convert.ToChar(Console.ReadLine());
+    }
+    static double Calculate(double a, double b, char op)
+    {
+        if (op == '+')
         {
-            //sdf
+            return a + b;
+        }
+
+        else if (op == '-')
+        {
+            return a - b;
+        }
+
+        else if (op == '*')
+        {
+            return a * b;
+        }
+
+        else if (op == '/')
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return 0;
+            }
+
+            return a / b;
+        }
+
+        else
+        {
+            Console.WriteLine("Неизвестная операция!");
+            return 0;
         }
     }
+    static void PrintResult(double a, double b, char op, double result)
+    {
+        Console.WriteLine($"Результат: {a} {op} {b} = {result}");
+    }
 }
-

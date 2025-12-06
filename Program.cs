5dd9a94aaafb008c3b637b2d4d406c7fceddd80b
student <student@DESKTOP-PFLTMIS>
2025-12-06 14:50:53 +0500

    Lesson

--- Program.cs
+++ Program.cs
@@ -8,10 +8,32 @@ namespace p511_oop
 {
     internal class Program
     {
+        //Наследование (Принцип ООП)
+        class Animal
+        {
+            public string Name { get; set; }
+            public Animal(string name)
+            {
+                Name = name;
+            }
+            public void Eat()
+            {
+                Console.WriteLine("Животное ест");
+            }
+        }
+
+        class Dog : Animal 
+        {
+            public Dog(string name) : base(name)
+            {
+                
+            }
+
+        }
+
 
         static void Main(string[] args)
-        {
-            //sdf
+        { 
         }
     }
     

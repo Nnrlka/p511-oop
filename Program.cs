8710a19a6b2a9e82e26f0afdfa447dda7323ac3e
student <student@DESKTOP-PFLTMIS>
2025-12-13 13:17:29 +0500

    111

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace p511_oop
 {
+    abstract class Human
+    {
+        public string Name { get; set; }
+        public string WorkPlace { get; set; }
+        public string Surname { get; set; }
+   
+    public class Builder : Human
+        {
+
+        }
     internal class Program
     {
 

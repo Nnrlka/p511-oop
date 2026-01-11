d79a2733df57fad76d86036e6062105b855eb4a1
student <student@DESKTOP-PFLTMIS>
2026-01-11 13:13:57 +0500

    assdasd

--- Program.cs
+++ Program.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace p511_oop
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            //sdf
+            Person person = new Person
+            {
+                Name = "john",
+                Age = 30
+            };
+            // Создание JSON
+            string json = JsonSerializer.Serialize(person);
+            Console.WriteLine(json);
+            //Преобразование из JSON в объект 
+            Person deserialized = JsonSerializer.Deserialize < Person>(json);
+
         }
     }
     
